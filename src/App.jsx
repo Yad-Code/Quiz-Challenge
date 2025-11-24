843c44fa9f4bcfa9dfd2891800e8ad654c78f2b5
Yad-Code <yad.4322@gmail.com>
2025-11-24 15:45:16 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Menu from "./components/Menu.jsx";
+import "./App.css";
+
+function App() {
+  //------------------The Questions------------------
+
+  //-----------Random array number------------
+
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
